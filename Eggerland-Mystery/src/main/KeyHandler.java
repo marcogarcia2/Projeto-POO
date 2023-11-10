22ecf620504c3a2c4bed7b4c755c516007106987
marcogarcia2 <mmgaspargarcia@gmail.com>
2023-11-10 15:45:36 -0300

    tiro funcionando (sem colisão)

--- Eggerland-Mystery/src/main/KeyHandler.java
+++ Eggerland-Mystery/src/main/KeyHandler.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
 
-	public boolean upPressed, downPressed, leftPressed, rightPressed;
+	public boolean upPressed, downPressed, leftPressed, rightPressed, spacePressed;
 	
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -31,8 +31,10 @@ public class KeyHandler implements KeyListener {
 		if(code == KeyEvent.VK_D) {
 			rightPressed = true;
 		}
-		
-		
+		if(code == KeyEvent.VK_SPACE) {
+			spacePressed = true;
+			System.out.println("espaço");
+		}
 	}
 
 	@Override
@@ -51,6 +53,9 @@ public class KeyHandler implements KeyListener {
 		}
 		if(code == KeyEvent.VK_D) {
 			rightPressed = false;
+		}
+		if(code == KeyEvent.VK_SPACE) {
+			spacePressed = false;
 		}
 	}
 
